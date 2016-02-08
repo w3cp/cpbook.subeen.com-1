04d2cf2a27107183fe4fb568800c5e7891383875
w3cp <jannat.books@gmail.com>
2016-02-08 17:39:43 +0000

    adding more files

--- ch14-lec06-file-struct/11-pattern.c
+++ ch14-lec06-file-struct/11-pattern.c
@@ -1,4 +1,3 @@
-
 /* 3^n = ***x, print x.
 exg: 3^6 = 729, print 9; 3^3 = 27, print 7 */
 
@@ -6,25 +5,18 @@ exg: 3^6 = 729, print 9; 3^3 = 27, print 7 */
 
 int main()
 {
-    // freopen("/sdcard/coding/oj/in.txt", "rt", stdin);
-    // freopen("/sdcard/coding/oj/out.txt", "wt", stdout);
+    // freopen("/sdcard/coding/anwar/oj/in.txt", "rt", stdin);
+    // freopen("/sdcard/coding/anwar/oj/out.txt", "wt", stdout);
     
     // 3^0 = 1, 3^1 = 3, 3^2 = 9, 3^3 = 27, 3^4 = 81, 3^5 = 243, 3^6 = 729 ...
     // 1 3 9 7 1 3 9 .... (pattern/cycle) 
     // 100 % 4 = 0, 6 % 4 = 2, 5 % 4 = 1, 3 % 4 = 3, 4 % 4 = 0, 2 % 4 = 2 
     
-    int pow;
-    int tc; // testcase
-    int i; // loop
+    int n;
     int values[4] = {1, 3, 9, 7};
-    
-    scanf("%d", &tc);
-    
-    for (i = 0; i < tc; i++) {
-        scanf("%d", &pow);
-        pow = pow % 4;
-        printf("%d\n", values[pow]);
-    }
+    scanf("%d", &n);
+    n = n % 4;
+    printf("%d\n", values[n]);
     
     return 0;
 }
